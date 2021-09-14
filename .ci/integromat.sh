b818d72f7c5db0c3057c4e10534aa11b0dfe7a43
srz_zumix <zumix.cpp@gmail.com>
2021-09-14 19:19:37 +0900

    fix lint

--- .ci/integromat.sh
+++ .ci/integromat.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 set -eu
 
 BASEDIR=$(dirname $0)
 
-if [ ! -f ${BASEDIR}/ci-normalize-envvars/ci-env.sh ]; then
-    git clone -b fix/unbound https://github.com/srz-zumix/ci-normalize-envvars.git ${BASEDIR}/ci-normalize-envvars
+if [ ! -f "${BASEDIR}/ci-normalize-envvars/ci-env.sh" ]; then
+    git clone -b fix/unbound https://github.com/srz-zumix/ci-normalize-envvars.git "${BASEDIR}/ci-normalize-envvars"
 fi
-. ${BASEDIR}/ci-normalize-envvars/ci-env.sh
+. "${BASEDIR}/ci-normalize-envvars/ci-env.sh"
 
 # DATE=$(TZ="Asia/Tokyo" date)
 DATE=$(date -u)
@@ -14,4 +16,4 @@ curl \
   -H "Content-Type: application/json" \
   -X POST \
   -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_ENV_NAME}\", \"commit\": \"${CI_ENV_GIT_COMMIT}\" }" \
-  ${INTEGROMAT_WEBHOOK_URL}
+  "${INTEGROMAT_WEBHOOK_URL}"
