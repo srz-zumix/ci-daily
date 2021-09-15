e8354665e6f40820d9fa5ffcaa806edfa365830f
srz_zumix <zumix.cpp@gmail.com>
2021-09-15 11:35:23 +0900

    fix branch

--- .ci/integromat.sh
+++ .ci/integromat.sh
@@ -5,7 +5,7 @@ set -eu
 BASEDIR=$(dirname $0)
 
 if [ ! -f "${BASEDIR}/ci-normalize-envvars/ci-env.sh" ]; then
-    git clone -b fix/unbound https://github.com/srz-zumix/ci-normalize-envvars.git "${BASEDIR}/ci-normalize-envvars"
+    git clone https://github.com/srz-zumix/ci-normalize-envvars.git "${BASEDIR}/ci-normalize-envvars"
 fi
 . "${BASEDIR}/ci-normalize-envvars/ci-env.sh"
 
