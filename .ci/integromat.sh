c36f6e63bee866d120ac63ef00a77ca5340c4867
srz_zumix <zumix.cpp@gmail.com>
2021-09-15 22:24:55 +0900

    travis ci (#1)

---
+++ .ci/integromat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+BASEDIR=$(dirname $0)
+
+if [ ! -f "${BASEDIR}/ci-normalize-envvars/ci-env.sh" ]; then
+    git clone https://github.com/srz-zumix/ci-normalize-envvars.git "${BASEDIR}/ci-normalize-envvars"
+fi
+. "${BASEDIR}/ci-normalize-envvars/ci-env.sh"
+
+# DATE=$(TZ="Asia/Tokyo" date)
+DATE=$(date -u)
+
+curl \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_ENV_NAME}\", \"commit\": \"${CI_ENV_GIT_COMMIT}\" }" \
+  "${INTEGROMAT_WEBHOOK_URL}"
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ci-daily
+
 CI service daily tasks
+
+[![Travis CI Build Status](https://app.travis-ci.com/srz-zumix/ci-daily.svg?branch=main)](https://app.travis-ci.com/srz-zumix/ci-daily)
+
+## Repository for CI service specification survey
+
+* [ci-specs](https://github.com/srz-zumix/ci-specs)
