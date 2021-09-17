cb602a9a8f1ce9f962cfcf9c07d036669b6b913a
srz_zumix <zumix.cpp@gmail.com>
2021-09-18 01:09:04 +0900

    add jfrog (#2)

--- .ci/integromat.sh
+++ .ci/integromat.sh
@@ -4,6 +4,8 @@ set -eu
 
 BASEDIR=$(dirname $0)
 
+env
+
 if [ ! -f "${BASEDIR}/ci-normalize-envvars/ci-env.sh" ]; then
     git clone https://github.com/srz-zumix/ci-normalize-envvars.git "${BASEDIR}/ci-normalize-envvars"
 fi
@@ -11,9 +13,10 @@ fi
 
 # DATE=$(TZ="Asia/Tokyo" date)
 DATE=$(date -u)
+NAME=${CI_NAME:-${CI_ENV_NAME}}
 
 curl \
   -H "Content-Type: application/json" \
   -X POST \
-  -d "{\"time\": \"${DATE}\", \"ci\": \"${CI_ENV_NAME}\", \"commit\": \"${CI_ENV_GIT_COMMIT}\" }" \
+  -d "{\"time\": \"${DATE}\", \"ci\": \"${NAME}\", \"commit\": \"${CI_ENV_GIT_COMMIT}\" }" \
   "${INTEGROMAT_WEBHOOK_URL}"
